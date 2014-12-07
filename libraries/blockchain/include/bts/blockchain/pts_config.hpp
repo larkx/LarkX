940af6114de73fba8260b3bfa710e3a8304b6210
Peter Conrad <conrad@quisquis.de>
2014-12-07 18:49:27 +0100

    Increased default fees + adapted test cases

--- libraries/blockchain/include/bts/blockchain/pts_config.hpp
+++ libraries/blockchain/include/bts/blockchain/pts_config.hpp
@@ -1,5 +1,5 @@
 #define PTS_INITIAL_MIN_LENGTH (5)
-#define PTS_ACCOUNT_REG_FEE (10000000)
+#define PTS_ACCOUNT_REG_FEE (10000000) // FIXME: increase for dry run #
 
 #define PTS_SUPPRESS_ASSETS     // undefine to allow asset creation/manipulation
 
