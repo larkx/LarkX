b74c3586cd2b7add74e1877af548805e100afc2e
Vikram Rajkumar <vikram@bitshares.org>
2014-12-11 18:15:44 -0500

    Duplicate transaction hardfork
    Conflicts:
    	libraries/blockchain/include/bts/blockchain/fork_blocks.hpp

--- libraries/blockchain/include/bts/blockchain/fork_blocks.hpp
+++ libraries/blockchain/include/bts/blockchain/fork_blocks.hpp
@@ -5,7 +5,7 @@
 
 #define BTSX_MARKET_FORK_TO_UNIX_TIME_LIST
 
-#define BTS_CHECK_CANONICAL_SIGNATURE_FORK_BLOCK_NUM BTS_V0_4_25_FORK_BLOCK_NUM
+#define BTS_CHECK_CANONICAL_SIGNATURE_FORK_BLOCK_NUM 3500 // FIXME
 
 namespace bts { namespace blockchain {
   uint32_t estimate_last_known_fork_from_git_revision_timestamp(uint32_t revision_time);
