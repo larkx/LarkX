55b8067c2249b6bd816741eb586f0a3052096162
Peter Conrad <conrad@quisquis.de>
2014-12-15 18:32:26 +0100

    Fixed build

--- libraries/blockchain/include/bts/blockchain/fork_blocks.hpp
+++ libraries/blockchain/include/bts/blockchain/fork_blocks.hpp
@@ -5,7 +5,9 @@
 
 #define BTSX_MARKET_FORK_TO_UNIX_TIME_LIST
 
-#define BTS_CHECK_CANONICAL_SIGNATURE_FORK_BLOCK_NUM 3500 // FIXME
+#define FORK_25 3500 // FIXME
+
+#define BTS_CHECK_CANONICAL_SIGNATURE_FORK_BLOCK_NUM FORK_25
 
 namespace bts { namespace blockchain {
   uint32_t estimate_last_known_fork_from_git_revision_timestamp(uint32_t revision_time);
